4841cd0af0b4e643048155a4e54afad419c344ad
AbrahamRH <abraham.esc.tra@gmail.com>
2020-09-15 00:31:00 -0500

    Se inicio la leccion de strings

---
+++ Curso+JS+Moderno/Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,9 @@
+let cadenao = "cadena pez";
+
+console.log(cadenao.length);
+
+//Encuentra la posicion de la primera letra, y devuelve un -1 si no la encuentra
+console.log(cadenao.indexOf("pez"));
+
+//Devuelve true o false.
+console.log(cadenao.includes("pez"));
